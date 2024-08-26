57a5268a3da507b45a8b5ccafba5e76dd8251b0f
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-08-26 15:57:26 +0200

    installed bootstrap

---
+++ pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function HomePage(){
+    return(
+        <div className="homepage">
+            <div class="container text-center">
+  <div class="row">
+    <div class="col">
+      1 of 2
+    </div>
+    <div class="col">
+      2 of 2
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col">
+      2 of 3
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
+        </div>
+    );
+}
