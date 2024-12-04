8131d09aa631ca9ac8b7a64b9bf1d6c50a26ae95
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-12-04 02:11:53 +0200

    removed useless files

--- pages/AddRecipePage.jsx
+++ pages/AddRecipePage.jsx
@@ -1,7 +1,6 @@
 import React, {useState, useEffect} from 'react';
-import { Table, Button } from 'semantic-ui-react';
 import axios from 'axios';
-import { useNavigate, Link} from 'react-router-dom';
+//import { useNavigate, Link} from 'react-router-dom';
 
 export default function AddRecipePage(){
 
@@ -99,71 +98,6 @@ const onDelete = (id) => {
               <button onClick={AddRecipe} type="submit">Add Recipe</button>
               <button onClick={() => setData(data)}>Update</button>
         </form>
-
-        {/* <Table.Body>
-  {apiData.map((data) => {
-     return (
-       <><Table.Row>
-         <Table.Cell>{data.recipeName}</Table.Cell>
-         <Table.Cell>{data.Ingredients}</Table.Cell>
-         <Table.Cell>{data.instructions}</Table.Cell>
-         <Table.Cell>{data.category}</Table.Cell>
-         <Table.Cell>{data.preparationTime}</Table.Cell>
-         <Table.Cell>{data.cookingTime}</Table.Cell>
-         <Table.Cell>{data.servings}</Table.Cell>
-         <Link to='/Update'>
-           <Table.Cell>
-             <Button>Update</Button>
-           </Table.Cell>
-         </Link>
-       </Table.Row><Button onClick={() => onDelete(data.id)}>Delete</Button></>
-   )})}
-</Table.Body>
-<AddRecipe/> */}
     </div>
   )
 }
-
-
-
-
-/*import React, {useState} from 'react';
-import axios from 'axios';
-
-export default function AddRecipePage(){
-
-  const AddRecipe =(event) =>{
-    event.preventDefault()
-    axios.post(`http://localhost:3000/recipes`, {
-      recipeName,
-      Ingredients,
-      instructions,
-      category,
-      preparation,
-      cookingTime,
-      servings
-  })
-  }
-  
-  const [recipeName, setRecipeName] = useState('');
-    const [Ingredients, setIngredients] = useState('');
-    const [category, setCategory] = useState('');
-    const [preparationTime, setPreparationTime] = useState('');
-    const [cookingTime, setCookingTime] = useState('');
-    const [servings, setServings] = useState('');
-
-  return(
-    <div className="addrecipe">
-      <form>
-            <input type="text" placeholder="Add Recipe name" onChange={(e) => setRecipeName(e.target.value)}/><br/>
-            <input type="text" placeholder="Ingredients (separated by commas)"onChange={(e) => setFirstName(e.target.value)}/><br/>
-            <input type="text" placeholder="Instructions"onChange={(e) => setIngredients(e.target.value)}/><br/>
-            <input type="text" placeholder="Category"onChange={(e) => setCategory(e.target.value)}/><br/>
-            <input type="text" placeholder="Preparation Time"onChange={(e) => setPreparationTime(e.target.value)}/><br/>
-            <input type="text" placeholder="Cooking Time (mins)"onChange={(e) => setCookingTime(e.target.value)}/><br/>
-            <input type="text" placeholder="Servings"onChange={(e) => setServings(e.target.value)}/><br/>
-            <button onClick={AddRecipe} type="submit">Add Recipe</button>
-        </form>
-    </div>
-  )
-}*/
